f224dd3ea5c5c67212c4814b8fb1d44d2f3d8ba0
a.panfilov <a.panfilov@225-IT-PC>
2024-05-17 18:57:55 +0400

    work migrating

--- ProjectManagementSystem/ProjectManagementSystem.Domain/Students/Student.cs
+++ ProjectManagementSystem/ProjectManagementSystem.Domain/Students/Student.cs
@@ -8,9 +8,9 @@ public sealed class Student : Entity<StudentId>
     private Student()
     { }
 
-    public string Firstname { get; private set; }
-    public string Middlename { get; private set; }
-    public string Lastname { get; private set; }
+    public string FirstName { get; private set; }
+    public string MiddleName { get; private set; }
+    public string LastName { get; private set; }
     public bool Graduated { get; private set; }
     public Group Group { get; private set; }
     public string Email { get; private set; }
