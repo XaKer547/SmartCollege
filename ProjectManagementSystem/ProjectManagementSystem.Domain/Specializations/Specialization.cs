707a6f573a88b3e609d7becbdbc943b325a79747
user <user@DESKTOP-FTTFP6B>
2024-05-19 21:35:56 +0400

    deleted trash

--- ProjectManagementSystem/ProjectManagementSystem.Domain/Specializations/Specialization.cs
+++
@@ -1,9 +0,0 @@
-﻿using SharedKernel;
-
-namespace ProjectManagementSystem.Domain.Specializations;
-
-public sealed class Specialization : Entity<SpecializationId>
-{
-    private Specialization() { }
-    public string Name { get; private set; }
-}
